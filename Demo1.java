ecf5206b3162e30b42b69780c4f8fb01b4ff4863
Aishwarya-devops <aiwspace@gmail.com>
2021-05-24 09:51:34 +0000

    First commit by developerB

--- Demo1.java
+++ Demo1.java
@@ -1,6 +1,7 @@
 
 class Demo1 {
     public static void main(String[] args) {
+       //Added by DeveloperB
         System.out.println("This is Demo1"); 
     }
 }
--- Demo2.java
+++ Demo2.java
@@ -1,6 +1,7 @@
 
 class Demo2 {
     public static void main(String[] args) {
+       //Added by DeveloperB
         System.out.println("This is Demo2"); 
     }
 }
