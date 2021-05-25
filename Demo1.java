b5ecfb8b4ba56f84bd9034543f250ae54f037ed8
Aishwarya-devops <aiwspace@gmail.com>
2021-05-25 10:22:29 +0000

    Resolved conlict

--- Demo1.java
+++ Demo1.java
@@ -3,5 +3,7 @@ class Demo1 {
     public static void main(String[] args) {
         System.out.println("This is Demo1_Branch1"); 
         System.out.println("This is Demo1_Branch2"); 
+        System.out.println("This is Demo1 with feature1"); 
+
     }
 }
